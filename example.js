ce86fe26ca0d98451ae95e9efe514fe2abd3d413
ryan anzer <ryan.anzer@epitech.eu>
2019-01-02 21:51:52 +0100

    update example

--- example.js
+++ example.js
@@ -1,8 +1,8 @@
 
-const svg2img = require('./index.js'),
+const convert = require('./index.js'),
 	fs = require('fs.promisify');
 
-convert('./test/fy.svg', {width: 100, height: 100}).then((res) => {
+convert('./test/3.svg', {width: 100, height: 100}).then((res) => {
 	console.log(res);
-	return fs.writeFile('out.png', res);
+	return fs.writeFile('3.png', res);
 });
